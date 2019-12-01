c27892bc2d3bba1e165172bca8454a566c215061
ZackMinott <zacharyminott1997@gmail.com>
2019-12-01 08:30:47 -0800

    Created a custom sign in button that can be used across the entirety of the application

--- rose-apparel/src/components/sign-in/sign-in.component.jsx
+++ rose-apparel/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -31,12 +34,11 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" value={this.state.email} onChange={this.handleChange} required />
-                    <label>Email</label>
-                    <input name="password" type="password" value={this.state.password} onChange={this.handleChange} required />
-                    <label>Password</label>
+                    <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label='email' required />
 
-                    <input type="submit" value="Submit Form" />
+                    <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label='password' required />
+
+                    <CustomButton type="submit">Sign In</CustomButton>/>
                 </form>
             </div>
         );
