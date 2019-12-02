8ef6edead1e2522e16cd10b7d385ba674441e096
ZackMinott <zacharyminott1997@gmail.com>
2019-12-01 20:13:53 -0800

    Finished styling the sign in portion of the sign in page

--- rose-apparel/src/components/sign-in/sign-in.component.jsx
+++ rose-apparel/src/components/sign-in/sign-in.component.jsx
@@ -39,8 +39,11 @@ class SignIn extends React.Component {
 
                     <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label='password' required />
 
-                    <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>{''}Sign In with Google{''}</CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+                    </div>
+                    
                 </form>
             </div>
         );
