b8f00590dd9ff1cd55571b28c8d6a4475fdde3e4
ZackMinott <zacharyminott1997@gmail.com>
2019-12-04 22:13:18 -0800

    updating our app to redirect to home if user is signedin

--- rose-apparel/src/redux/user/user.actions.js
+++ rose-apparel/src/redux/user/user.actions.js
@@ -1,6 +1,6 @@
-//import userReducer from "./user.reducer";
+import { UserActionTypes } from './user.types';
 
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 });
