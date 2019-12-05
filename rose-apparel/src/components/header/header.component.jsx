8c61757763667c4ab40290834ac303b9eb2c102e
ZackMinott <zacharyminott1997@gmail.com>
2019-12-04 21:57:38 -0800

    added redux, and implemented userReducer and userActions to header and app components

--- rose-apparel/src/components/header/header.component.jsx
+++ rose-apparel/src/components/header/header.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+
 
 import { ReactComponent as Logo } from '../../assets/rose.svg';
 import {auth} from '../../firebase/firebase.utils';
@@ -27,6 +29,10 @@ const Header = ({currentUser}) => (
             }
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser 
+});
+
+export default connect(mapStateToProps)(Header);
